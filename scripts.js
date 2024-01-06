0022b6fba7f5be1a0b933cf0b5e226f8c2417831
martin. <bright-spark@theradio.fm>
2024-01-06 15:41:33 +0200

    reset

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 // Array of prepopulated strings
 var strings = [
-  //https://www.youtube-nocookie.com/embed/videoseries?list=
-  //"PLuWIxgNYqm0ketpCdUpLK4zjreYpDlLoE&enablejsapi=1",
-  //"PLuWIxgNYqm0lwRXD3Sqe2_ZryiljuW-L9&enablejsapi=1",
-  //"PLuWIxgNYqm0lnbXjnFF3bdEBbRHkr-R3k&enablejsapi=1",
-  //"PLuWIxgNYqm0mQd_Z6eYbNa8Riyjl0znGC&enablejsapi=1",
-  //"PLuWIxgNYqm0lXxPLZ9hBT9M46m53H5GES&enablejsapi=1"
-  "https://google.com"
+  "PLuWIxgNYqm0ketpCdUpLK4zjreYpDlLoE&enablejsapi=1",
+  "PLuWIxgNYqm0ketpCdUpLK4zjreYpDlLoE&enablejsapi=1",
+  "PLuWIxgNYqm0lwRXD3Sqe2_ZryiljuW-L9&enablejsapi=1",
+  "PLuWIxgNYqm0lnbXjnFF3bdEBbRHkr-R3k&enablejsapi=1",
+  "PLuWIxgNYqm0mQd_Z6eYbNa8Riyjl0znGC&enablejsapi=1",
+  "PLuWIxgNYqm0lXxPLZ9hBT9M46m53H5GES&enablejsapi=1"
+  //"example.com"
 ];
 
 function generateRandomPlaylist() {
@@ -17,7 +17,7 @@ function generateRandomPlaylist() {
   var randomPlaylist = strings[randomIndex];
   
   // Set the src attribute of the iframe to the random string
-  document.getElementById("play").src = "" + randomPlaylist;
+  document.getElementById("play").src = "https://www.youtube-nocookie.com/embed/videoseries?list=" + randomPlaylist;
 }
 
 // Call the function when the page loads
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'riptide-v1';
+const CACHE_NAME = 'riptide-v1.1';
 const urlsToCache = [
   '/',
   '/index.html',
