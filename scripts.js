6535bebbb241026cfdd28205121d97b1f82a90f7
martin. <bright-spark@theradio.fm>
2024-01-07 06:58:26 +0200

    random seed and then next

--- scripts.js
+++ scripts.js
@@ -1,28 +1,50 @@
 // Array of prepopulated strings
 var strings = [
   "https://www.youtube-nocookie.com/embed/videoseries?list=PLuWIxgNYqm0ketpCdUpLK4zjreYpDlLoE&enablejsapi=1",
-  //"PLuWIxgNYqm0ketpCdUpLK4zjreYpDlLoE&enablejsapi=1",
-  //"PLuWIxgNYqm0ketpCdUpLK4zjreYpDlLoE&enablejsapi=1",
-  //"PLuWIxgNYqm0lwRXD3Sqe2_ZryiljuW-L9&enablejsapi=1",
-  //"PLuWIxgNYqm0lnbXjnFF3bdEBbRHkr-R3k&enablejsapi=1",
-  //"PLuWIxgNYqm0mQd_Z6eYbNa8Riyjl0znGC&enablejsapi=1",
-  //"PLuWIxgNYqm0lXxPLZ9hBT9M46m53H5GES&enablejsapi=1"
+  "https://www.canva.com/design/DAF4kU195is/HKaAbVXdekY1F7zSJRx6zw/watch?embed",
   "https://example.com",
-  "https://youtube.com",
-  "https://m.youtube.com"
+  "https://redbuilder.dev"
 ];
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+function setNextPlaylistID() {
+  var currentIndex = strings.indexOf(getCookie("playlistID"));
+
+  // If the cookie exists and its value is one of the playlist IDs in the array
+  if (currentIndex !== -1) {
+    // Pick the next playlist ID in the array (loop back to the first one if necessary)
+    var nextIndex = (currentIndex + 1) % strings.length;
+    var nextPlaylist = strings[nextIndex];
+
+    // Save the next playlist ID as the cookie
+    document.cookie = "playlistID=" + nextPlaylist + "; path=/";
+    // Open playlist
+    document.getElementById("play").src = nextPlaylist;
+  } else {
+    // If the cookie doesn't exist or its value is not in the array, generate a random playlist as before
+    generateRandomPlaylist();
+  }
+}
 
 function generateRandomPlaylist() {
   // Get a random index from the strings array
   var randomIndex = Math.floor(Math.random() * strings.length);
-  
+
   // Get the string at the random index
   var randomPlaylist = strings[randomIndex];
-  
-  // Set the src attribute of the iframe to the random string
-  // document.getElementById("play").src = "https://www.youtube-nocookie.com/embed/videoseries?list=" + randomPlaylist;
+
+  // Save the playlist ID as a cookie
+  document.cookie = "playlistID=" + randomPlaylist + "; path=/";
+
+  // Set the src attribute of the iframe to the random playlist
+  // document.getElementById("playlistiframe").src = "https://www.youtube-nocookie.com/embed/videoseries?list=" + randomPlaylist;
   document.getElementById("play").src = randomPlaylist;
 }
 
-// Call the function when the page loads
-window.addEventListener("load", generateRandomPlaylist);
+// Call setNextPlaylistID() when the page loads
+window.addEventListener("load", setNextPlaylistID);
